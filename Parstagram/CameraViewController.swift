ee021ca4a373ad237018af5171582dbface8c5d2
shriprama <shriprama@gmail.com>
2019-03-27 15:21:56 -0500

    final

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,68 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Shriprama Rao on 3/7/19.
+//  Copyright © 2019 Shriprama Rao. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var cameraImageView: UIImageView!
+    
+    @IBOutlet weak var commentField: UITextField!
+    @IBAction func onSubmit(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = commentField.text!
+        post["author"] = PFUser.current()!
+        
+        let imageData = cameraImageView.image?.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Image saved")
+            } else {
+                print("Error")
+            }
+            
+        }
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        
+        let scaledImage = image.af_imageAspectScaled(toFit: size)
+        cameraImageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+        
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,56 @@
+# Parstagram - Part II
+
+This is an Instagram clone with a custom Parse backend that allows a user to post photos, view a global photos feed, and add comments!
+
+Time spent: **6** hours spent in total
+
+## User Stories
+
+#### REQUIRED (10pts)
+
+- [x] User stays logged in across restarts. (1pt)
+- [x] User can log out. (1pt)
+- [x] User can view comments on a post. (3pts)
+- [x] User can add a new comment. (5pts)
+
+#### BONUS
+
+- [ ] User can add a profile picture. (2pts)
+- [ ] Profile pictures are shown for posts and comments. (2pts)
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://g.recordit.co/qRMrhbHcHY.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+---
+
+# Parstagram - Part I
+
+Time spent: **6** hours spent in total
+
+### User Stories
+
+#### REQUIRED (10pts)
+
+The following **required** functionality is completed:
+
+- [x] User sees app icon in home screen and styled launch screen. (1pt)
+- [x] User can sign up to create a new account. (1pt)
+- [x] User can log in. (1pt)
+- [x] User can take a photo, add a caption, and post it to the server. (3pt)
+- [x] User can view the last 20 posts. (4pts)
+
+#### BONUS
+
+- [x ] User can pull to refresh. (1pt)
+- [ ] User can load past posts infinitely. (2pts)
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://g.recordit.co/wSZZTyKa7Q.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [Recordit](http://recordit.co/).
